f7163b78dd3ec12d17731b62202d48e62fe25c77
Alain <alain.tramont@deepesg.com>
2025-10-26 16:52:35 -0300

    next

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,43 @@
-
+import { mergeConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs"
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs'
   ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [vue(), vueJsx()],
+      esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.jsx?$/,
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+          loader: {
+            '.js': 'jsx',
+            '.jsx': 'jsx',
+          },
+        },
+      },
+    });
+  },
 };
+
 export default config;
