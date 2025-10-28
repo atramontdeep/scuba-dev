416054fda7c329c4b30f7cd0bfd1ce6f60ce6570
Alain <alain.tramont@deepesg.com>
2025-10-28 10:15:16 -0300

    mjs

---
+++ .storybook/main.mjs
@@ -0,0 +1,46 @@
+import { mergeConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs'
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  features: {
+    buildStoriesJson: true
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [vue(), vueJsx()],
+      define: {
+        'process.env': {},
+        global: 'globalThis',
+      },
+      resolve: {
+        alias: {
+          '@': '/src',
+        },
+      },
+      optimizeDeps: {
+        include: ['vue'],
+      },
+    });
+  },
+};
+
+export default config;
